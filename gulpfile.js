32901d1ada546e4430c13a55d0835b21e151a80a
Malitha Liyanage <malitharanu@gmail.com>
2022-02-19 22:48:38 +0530

    CSL-001 - Deleted unwanted files and reconfigured gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('sass/**/*.scss')
+  return src('library/**/*.scss')
     .pipe(sass())
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss', '*.html'], buildStyles)
+  watch(['library/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
